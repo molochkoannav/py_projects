feca31a2d7271a9befb676f3ffb612965dae3db5
Ann Molochko <molochkoannav@gmail.com>
2026-06-19 11:06:10 +0300

    hw14_2 add new methods in module

--- src/category.py
+++ src/category.py
@@ -1,13 +1,39 @@
+from src.product import Product
+
+
 class Category:
+    """Класс категорий продуктов"""
     name: str
     description: str
     products: list
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description,products):
         self.name = name
         self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count += len(products)
+        self.__products = products if products else []
+        Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product):
+        """ Метод для добавления продуктов в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер для получения списка продуктов"""
+        return self.__products
+
+    @property
+    def printing_products_list(self):
+        """ Метод для вывода списка продуктов в категории"""
+        str_products = ""
+        for product in self.__products:
+            str_products += f"{product.name},  {product.price} руб. Остаток: {product.quantity} шт\n"
+        return str_products
+
+
+
+
--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+
+
+
 class Product:
     name: str
     description: str
@@ -7,5 +10,38 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data: dict, existing_products: list = None):
+        name = product_data.get('name')
+        description = product_data.get('description')
+        price = product_data.get('price')
+        quantity = product_data.get('quantity')
+        if existing_products is not None:
+            for product in existing_products:
+                if product.name.lower() == name.lower():
+                    product.quantity += quantity
+                    product.price = max(product.price, price)
+                    return product
+        return cls(name, description, price, quantity)
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        if new_price < self.__price:
+            answer_user = input(f"Понизить цену с {self.__price} до {new_price}? y/n ")
+            if answer_user.lower() == 'y':
+                self.__price = new_price
+                print(f"Цена успешно понижена до {new_price}")
+            else:
+                print("Изменение цены отменено")
+        else:
+            self.__price = new_price
